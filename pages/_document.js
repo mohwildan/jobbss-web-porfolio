c1f65f74153239a970ba7f67256146fcad3dd2a9
mohwildan <mobafreewtf@gmail.com>
2022-06-18 18:32:34 +0700

    first commt

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import Theme from '../Theme'
+import Theme from "../Theme"
 
 export default class Document extends NextDocument {
   render() {
